7a677ca79c0420a2b73526f0caf6d477f6334b2a
Alexis Toulotte <al@alweb.org>
2017-12-05 19:34:19 +1100

    Using private_class_method

--- lib/notu/html_document.rb
+++ lib/notu/html_document.rb
@@ -6,14 +6,13 @@ module Notu
       parse(HttpDownload.get(url, options))
     end
 
-    private
-
     def self.parse(data)
       data = data.gsub(/&nbsp;/i, ' ').gsub(/\s+/, ' ')
       document = Nokogiri::HTML.parse(data, nil, 'UTF-8')
       raise ParseError.new('Invalid HTML document') if (document/'head').empty?
       document
     end
+    private_class_method :parse
 
   end
 
